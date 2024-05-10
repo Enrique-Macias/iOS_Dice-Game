7edd5e741caae83edbf6852ac7d40c01905645ee
Enrique Macias Lopez <a01641402@tec.mx>
2024-05-10 10:50:33 -0600

    Initial commit

--- Dados/ViewController.swift
+++ Dados/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
+class InformationViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.layer.cornerRadius = 10
+        view.clipsToBounds = true
+    }
+}
+
 class ViewController: UIViewController {
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
+        
+        diceImageView1.image = UIImage(named: "dice1")
+        diceImageView2.image = UIImage(named: "dice1")
+        
     }
-
-
+   
+    @IBAction func addButton(_ sender: Any) {
+        let informationVC = InformationViewController()
+        informationVC.modalPresentationStyle = .overCurrentContext
+        present(informationVC, animated: true, completion: nil)
+    }
+    @IBAction func buttonPressed(_ sender: Any) {
+        let alert = UIAlertController(title: "Information", message: "This is the information displayed.", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceArray = [UIImage(named: "dice1"), UIImage(named: "dice2"), UIImage(named: "dice3"), UIImage(named: "dice4"), UIImage(named: "dice5"), UIImage(named: "dice6")]
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+    }
 }
 
